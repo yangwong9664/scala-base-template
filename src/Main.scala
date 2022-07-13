4a4075d0aef27715187fdd4158aad72119fd298e
yangwong9664 <20066247+yangwong9664@users.noreply.github.com>
2022-07-13 11:44:30 +0100

    test

--- src/Main.scala
+++ src/Main.scala
@@ -1,6 +1,14 @@
 import utils.Logging._
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 object Main extends App {
 
-  logInfo("hello")
+  def duplicateCount(str: String): Int = {
+    str.groupBy(_.toLower).filterNot(_._2.length <= 1).size
+  }
+
+  println(duplicateCount("abcdea"))
+
 
 }
